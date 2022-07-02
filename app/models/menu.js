0977dea6cf60f5551d2184608766763a37623362
Vinayak Banga <vinayakbanga22@gmail.com>
2022-07-03 01:42:49 +0530

    Category addded to menu scehma

--- app/models/menu.js
+++ app/models/menu.js
@@ -6,7 +6,8 @@ const menuSchema=new Schema({
     name:{type:String,required:true},
     image:{type:String,required:true},
     price:{type:String,required:true},
-    type:{type:String,required:true}
+    type:{type:String,required:true},
+    category:{type:String,required:true}
 })
 
 const Menu=mongoose.model('Menu',menuSchema)
